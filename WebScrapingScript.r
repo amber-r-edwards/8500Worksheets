aaf3af14bc6e2e4de603a91cd16c0a08914c4969
amber-r-edwards <arose52400@gmail.com>
2025-03-13 12:54:08 -0400

    txt files but separate dataframe than my metada?

---
+++ WebScrapingScript.r
@@ -0,0 +1,18 @@
+#Scraping Quest: A Feminist Quarterly from Internet Archive
+#Research Question - comparison with women's liberation zines, what the content of the quarterly may show about the differences between their respective authors and audiences/how much they overlap and what the role of activism looks like between zines that highlight active protest and a quarterly aiming toward intellectual discussion
+
+library(internetarchive)
+#add search results to an item and then use that item to get metadata of each item
+quest <- ia_keyword_search("collection:questquarterly", num_results = 20)
+questitems <- ia_get_items(quest, silence = FALSE)
+questmetadata <- ia_metadata(questitems)
+#download files - but I only want the text files
+questdownload <- ia_files(questitems) %>%
+    filter(type = "txt")
+
+ia_download(dir = "questfiles", extended_name = TRUE, overwrite = TRUE, silence = FALSE)
+
+#define volume?
+
+#create empty dataframe
+df <- data.frame(id = character())
