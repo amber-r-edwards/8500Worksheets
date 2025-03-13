1fd13498ec4051ee90b31ed1021deb0c7d79ef54
amber-r-edwards <arose52400@gmail.com>
2025-03-13 12:55:48 -0400

    to join or to redo?

--- WebScrapingScript.r
+++ WebScrapingScript.r
@@ -2,17 +2,16 @@
 #Research Question - comparison with women's liberation zines, what the content of the quarterly may show about the differences between their respective authors and audiences/how much they overlap and what the role of activism looks like between zines that highlight active protest and a quarterly aiming toward intellectual discussion
 
 library(internetarchive)
+library(tidyverse)
 #add search results to an item and then use that item to get metadata of each item
 quest <- ia_keyword_search("collection:questquarterly", num_results = 20)
 questitems <- ia_get_items(quest, silence = FALSE)
 questmetadata <- ia_metadata(questitems)
 #download files - but I only want the text files
 questdownload <- ia_files(questitems) %>%
-    filter(type = "txt")
+    filter(type == "txt") %>%
+    ia_download(dir = "questfiles", extended_name = TRUE, overwrite = TRUE, silence = FALSE)
 
-ia_download(dir = "questfiles", extended_name = TRUE, overwrite = TRUE, silence = FALSE)
-
-#define volume?
 
 #create empty dataframe
 df <- data.frame(id = character())
